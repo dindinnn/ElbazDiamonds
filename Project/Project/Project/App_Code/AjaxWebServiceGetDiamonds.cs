a5d7634f27f93c7cd0cb6405f0fa1d625e3e010b
dindinnn <33927264+dindinnn@users.noreply.github.com>
2018-07-24 22:05:56 +0300

    dina 24/7 22:05

---
+++ Project/Project/Project/App_Code/AjaxWebServiceGetDiamonds.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+
+/// <summary>
+/// Summary description for AjaxWebServiceGetDiamonds
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]  // REMEMBER TO UNCOMMENT THIS LINE
+public class AjaxWebServiceGetDiamonds : System.Web.Services.WebService
+{
+
+    public AjaxWebServiceGetDiamonds()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    //קבלת האבנים הקיימות במלאי
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+
+    public string getStonesByName(string name)
+    {
+        string stoneName = name;
+        Stone s = new Stone();
+        List<Stone> ls = s.getStonesByName(categoryId);
+
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        // serialize to string
+        string jsonStringCategory = js.Serialize(ls);
+        return jsonStringCategory;
+    }
+}
